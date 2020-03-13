acfd5542ac01fa6ad750ad405e53551956e49827
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-03-13 09:45:33 +0100

    chore: ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,6 +13,22 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
+    # loop through the weights
+    for i in range(0, length):
+      # the current weight is the weight at that index
+        current_weight = weights[i]
+        # use the hashtable insert function
+        hash_table_insert(ht, current_weight, i)
+        # get the index of limit - current weight
+        diff = limit - current_weight
+        result = hash_table_retrieve(ht, diff)
+        # since the order matters, check the size of the indices
+        # of the weights and return in the correct order The higher valued index should be placed in the `zeroth` index 
+        # and the smaller index should be placed in the `first` index
+        if result:
+            if length == 2:
+                return (1, 0)
+            return (i, result)
     return None
 
 
